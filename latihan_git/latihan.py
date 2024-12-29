7f917fdb8abcfb36c3c68986a1ebcd02a14647fa
Ilham.oss409 <ilham.abdul@mhs.itenas.ac.id>
2024-12-30 04:59:11 +0700

    Perubahan

--- latihan_git/latihan.py
+++ latihan_git/latihan.py
@@ -1,1 +1,13 @@
-print("Latihan")
+data_panen = {
+    'lokasi1': {'nama_lokasi': 'Kebun A', 'hasil_panen': {'padi': 1200, 'jagung': 800, 'kedelai': 500}},
+    'lokasi2': {'nama_lokasi': 'Kebun B', 'hasil_panen': {'padi': 1500, 'jagung': 900, 'kedelai': 450}},
+    'lokasi3': {'nama_lokasi': 'Kebun C', 'hasil_panen': {'padi': 1100, 'jagung': 750, 'kedelai': 600}},
+    'lokasi4': {'nama_lokasi': 'Kebun D', 'hasil_panen': {'padi': 1300, 'jagung': 850, 'kedelai': 550}},
+    'lokasi5': {'nama_lokasi': 'Kebun E', 'hasil_panen': {'padi': 1400, 'jagung': 950, 'kedelai': 480}}
+}
+
+
+for lokasi, info in data_panen.items():
+    print(f"nama Lokasi: {info['nama_lokasi']}, hasil Panen: {info['hasil_panen']}")
+
+print(f"hasil panen jagung di lokasi2: {data_panen['lokasi2']['hasil_panen']['jagung']}")
